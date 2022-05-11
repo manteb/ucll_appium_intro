05cc1c0c86ddfc1fdb4039999641fe0713529904
Mante Bos <mante.bos@gmail.com>
2022-05-11 16:51:33 +0200

    new exercise

--- AppiumIntroduction/LoginTest.cs
+++ AppiumIntroduction/LoginTest.cs
@@ -1,18 +1,41 @@
 ﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.MultiTouch;
 
-namespace AppiumIntroduction
+namespace AppiumTraining
 {
-    public class Tests
+    public class LoginTest
     {
+
+
+        private const string appiumUri = "http://0.0.0.0:4723/wd/hub";
+        private AndroidDriver<AndroidElement> driver;
+
         [SetUp]
         public void Setup()
         {
+            var options = new AppiumOptions();
+            options.PlatformName = "Android";
+            options.AddAdditionalCapability(MobileCapabilityType.App, "<linkTo.apk>");
+            driver = new AndroidDriver<AndroidElement>(new Uri(appiumUri), options);
         }
 
         [Test]
-        public void Test1()
+        public void Login()
         {
-            Assert.Pass();
+            //Find element username
+            //Find element password
+            //Find element login button
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
