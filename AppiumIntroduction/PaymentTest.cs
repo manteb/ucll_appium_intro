b1e4de1055293795a85abce88351dd98f5457b3a
Mante Bos <mante.bos@gmail.com>
2022-05-12 17:24:41 +0200

    added todos

--- AppiumIntroduction/PaymentTest.cs
+++ AppiumIntroduction/PaymentTest.cs
@@ -20,7 +20,7 @@ namespace AppiumTraining
         {
             var options = new AppiumOptions();
             options.PlatformName = "Android";
-            options.AddAdditionalCapability(MobileCapabilityType.App, "pathToApk");
+            options.AddAdditionalCapability(MobileCapabilityType.App, "PathToAPK");
             driver = new AndroidDriver<AndroidElement>(new Uri(appiumUri), options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
@@ -28,21 +28,69 @@ namespace AppiumTraining
         [Test]
         public void MakePayment()
         {
-            //TODO Login
-            //TODO Get the balance
-            //TODO Click Make payment
-            //TODO Fill in phone, name, amount
-            //TODO select country (no swipe-needed yet, take one of the first in the list)
-            //TODO Click Send payment
-            //TODO Click Yes
-            //TODO Get the new balance
-            //TODO verify that newBalance == balanceBefore - paymentAmount
+            var amount = 10;
+            Login();
+            var initialBalance = GetBalance();
+            driver.FindElementById("makePaymentButton").Click();
+
+            driver.FindElementById("phoneTextField").SendKeys("0499112233");
+            driver.FindElementById("nameTextField").SendKeys("Tobania");
+            driver.FindElementById("amountTextField").SendKeys(amount.ToString());
+            driver.FindElementById("countryButton").Click();
+
+
+            //TODO search for a country on the bottom of the list -> Swipe needed
+            var countries = driver.FindElementsByClassName("android.widget.TextView");
+            foreach (var country in countries)
+            {
+                if (country.Text.Equals("India"))
+                {
+                    country.Click();
+                    break;
+                }
+            }
+
+
+            driver.FindElementById("sendPaymentButton").Click();
+            driver.FindElementById("android:id/button1").Click();
+
+            var currentBalance = GetBalance();
+            Assert.AreEqual(initialBalance - amount, currentBalance);
         }
 
         [TearDown]
         public void TearDown()
         {
             driver.Quit();
+        }
+
+        private float GetBalance()
+        {
+            var balance_txt = driver.FindElementByClassName("android.view.View").Text;
+            var startIndex = balance_txt.IndexOf(":") + 2;
+            var endIndex = balance_txt.IndexOf("$") - 1;
+            String balance = balance_txt.Substring(startIndex, endIndex - startIndex + 1);
+            return float.Parse(balance);
+        }
+
+        private void Login()
+        {
+            driver.FindElementById("usernameTextField").SendKeys("company");
+            driver.FindElementById("passwordTextField").SendKeys("company");
+            driver.FindElementById("loginButton").Click();
+        }
+
+        private bool ElementExists(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (WebDriverException)
+            {
+                return false;
+            }
         }
     }
 }
